1cac2c0c9bbae68c33c2583fe8f0ec1ebb3bec5f
ChaseParate <58488365+ChaseParate@users.noreply.github.com>
2020-12-06 01:19:24 -0800

    Optimize Day 5

--- day05/part1.py
+++ day05/part1.py
@@ -1,11 +1,10 @@
 def binary_boarding(puzzle_input):
-    return max(partition_to_binary(partition)[0] * 8 +
-               partition_to_binary(partition)[1] for partition in puzzle_input)
+    return max(partition_to_binary(partition) for partition in puzzle_input)
 
 
 def partition_to_binary(partition):
-    return int(partition[:-3].replace('F', '0').replace('B', '1'), 2), \
-           int(partition[-3:].replace('L', '0').replace('R', '1'), 2)
+    return int(partition.replace('F', '0').replace('L', '0')
+               .replace('B', '1').replace('R', '1'), 2)
 
 
 with open('puzzle_input.txt') as f:
