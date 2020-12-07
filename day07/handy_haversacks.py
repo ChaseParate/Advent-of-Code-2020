732353c7f905bdf2a50a1227ae051e25b610b0e1
ChaseParate <58488365+ChaseParate@users.noreply.github.com>
2020-12-06 23:25:02 -0800

    Concatenate Day 7 Part Files

--- day07/handy_haversacks.py
+++ day07/handy_haversacks.py
@@ -1,3 +1,31 @@
+def part1(puzzle_input):
+    bags = {}
+    for bag in puzzle_input:
+        bag_type, contained_bags = bag.split(' contain ')
+
+        bag_type = bag_type[:-5]
+        contained_bags = contained_bags[:-1].replace('bags', 'bag').split(' bag, ')
+        contained_bags[-1] = contained_bags[-1][:-4]
+        
+        if 'no other' in contained_bags:
+            bags[bag_type] = contained_bags
+            continue
+
+        #print(bag_type)
+        #print(contained_bags)
+        #print()
+
+        bags[bag_type] = [b[2:] for b in contained_bags]
+
+    # do funny list comprehension
+    # revert to .values()?
+    good_bags = 0
+    for bag, bag_contents in bags.items():
+        if bags_containing_shiny_gold_bag(bags, bag_contents):
+            good_bags += 1
+    return good_bags
+
+
 def part2(puzzle_input):
     bags = {}
     for bag in puzzle_input:
@@ -19,6 +47,19 @@ def part2(puzzle_input):
 
     #print(bags['shiny gold'])
     return get_something_send_help(bags, bags['shiny gold'])
+
+
+def bags_containing_shiny_gold_bag(bags, bag_contents):
+    if 'shiny gold' in bag_contents:
+        return True
+    elif 'no other' in bag_contents:
+        return False
+    else:
+        return any(bags_containing_shiny_gold_bag(bags, bags[inner_bag]) for inner_bag in bag_contents)
+        #for inner_bag in bag_contents:
+            #print(bag_contents)
+            #print('help', inner_bag)
+            #print(get_something_send_help(bags, bags[inner_bag]))
 
 
 # edit function so that instead of returning T/F, returning number that eventually gets summed?
@@ -44,4 +85,5 @@ with open('input.txt') as f:
     data = f.read()
 
 puzzle_input = data.splitlines()
+print('Part 1:', part1(puzzle_input))
 print('Part 2:', part2(puzzle_input))
