09322dc8f6a17fd5a6d11bc81c3ddf9ed0a16e21
ChaseParate <58488365+ChaseParate@users.noreply.github.com>
2020-12-10 13:15:54 -0800

    Minorly Comment and Optimize Day 10

--- day10/adapter_array.py
+++ day10/adapter_array.py
@@ -6,6 +6,8 @@ def part1(puzzle_input):
     sorted_adapters = sorted(puzzle_input + [0, max(puzzle_input) + 3])
     difference = [sorted_adapters[i] - sorted_adapters[i-1] for i in
                   range(1, len(sorted_adapters))]
+    # [1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 3, ...]
+    
     return difference.count(1) * difference.count(3)
 
 
@@ -13,11 +15,15 @@ def part2(puzzle_input):
     sorted_adapters = sorted(puzzle_input + [0, max(puzzle_input) + 3])
     difference = [sorted_adapters[i] - sorted_adapters[i-1] for i in
                   range(1, len(sorted_adapters))]
-    difference_string = ''.join(str(num) for num in difference)
-    lengths = [len(sequence) for sequence in
-               re.findall('(1{2,})3', difference_string)]
+    # [1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 3, ...]
 
-    return prod([tribonacci_sequence(length) for length in lengths])
+    difference_string = ''.join(map(str, difference))
+    # '11113111131113...'
+
+    lengths = map(len, re.findall('(1{2,})3', difference_string))
+    # [4, 4, 3, ...]
+
+    return prod(map(tribonacci_sequence, lengths))
 
 
 def tribonacci_sequence(n):
